c32117f6c252c14cd1f80fbd8070d240857c5a8b
Miguel Teheran <miguelteheran@gmail.com>
2022-09-21 19:41:28 -0500

    working with ordinal numbers

--- humanizerdemo/Program.cs
+++ humanizerdemo/Program.cs
@@ -9,6 +9,7 @@ var salary = 1000;
 //Console.WriteLine($"My Name is {name} and I am {age.ToWords()} years old ");
 //Console.WriteLine($"Mi nombre es {name.Transform(To.TitleCase)} y tengo {age.ToWords(culture)} años ");
 Console.WriteLine($@"Mi nombre es {name.Transform(To.TitleCase)} 
-y tengo { "año".ToQuantity(age)} ");
-Console.WriteLine($"Y mi salario es { "dollar".ToQuantity(salary, "C2") }");
-
+, tengo { "año".ToQuantity(age)} ");
+Console.WriteLine($", mi salario es { "dollar".ToQuantity(salary, "C2") }");
+Console.WriteLine($", soy el { 2.ToOrdinalWords(culture) } de 2 hermanos");
+Console.WriteLine($"y mi hermana es la { 1.ToOrdinalWords(GrammaticalGender.Masculine, culture) } ");

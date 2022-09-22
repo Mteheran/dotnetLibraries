dc8bff7118046a4b82ec6d920d61a1ff063136ac
Miguel Teheran <miguelteheran@gmail.com>
2022-09-21 19:02:13 -0500

    working with culture

--- humanizerdemo/Program.cs
+++ humanizerdemo/Program.cs
@@ -1,6 +1,9 @@
-﻿using Humanizer;
+﻿using System.Globalization;
+using Humanizer;
 
+var culture = new CultureInfo("es-ES");
 var name = "Miguel Teheran";
 var age = 30;
 
-Console.WriteLine($"My Name is {name} and I am {age.ToWords()} years old ");
+//Console.WriteLine($"My Name is {name} and I am {age.ToWords()} years old ");
+Console.WriteLine($"Mi nombre es {name} y tengo {age.ToWords(culture)} años ");

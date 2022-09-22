c95c5c478c8df68c750d4cda1f817c6cf5086528
Miguel Teheran <miguelteheran@gmail.com>
2022-09-21 19:24:21 -0500

    using ToQuantity method

--- humanizerdemo/Program.cs
+++ humanizerdemo/Program.cs
@@ -3,7 +3,12 @@ using Humanizer;
 
 var culture = new CultureInfo("es-ES");
 var name = "miguel teheran";
-var age = 30;
+var age = 1;
+var salary = 1000;
 
 //Console.WriteLine($"My Name is {name} and I am {age.ToWords()} years old ");
-Console.WriteLine($"Mi nombre es {name.Transform(To.TitleCase)} y tengo {age.ToWords(culture)} años ");
+//Console.WriteLine($"Mi nombre es {name.Transform(To.TitleCase)} y tengo {age.ToWords(culture)} años ");
+Console.WriteLine($@"Mi nombre es {name.Transform(To.TitleCase)} 
+y tengo { "año".ToQuantity(age)} ");
+Console.WriteLine($"Y mi salario es { "dollar".ToQuantity(salary, "C2") }");
+

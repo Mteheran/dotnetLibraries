5ed6aeaca5c53d8b750e488fbdc043e119bf34e6
Miguel Teheran <miguelteheran@gmail.com>
2022-10-13 18:40:45 -0500

    frist demo using StreamReader

--- csvdemo/Program.cs
+++ csvdemo/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Data;
+using System.Globalization;
+using CsvHelper;
+
+using (var reader = new StreamReader("users.csv"))
+{
+    using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+    {
+        using(var dr = new CsvDataReader(csv))
+        {
+            var dt =  new DataTable();
+            dt.Load(dr);
+
+            Console.WriteLine($"{dt.Rows[0][0]}");
+        }
+    }
+}

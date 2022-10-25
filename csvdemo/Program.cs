9bf795388a102e32b90c2284348b6f257719a299
Miguel Teheran <miguelteheran@gmail.com>
2022-10-25 17:03:11 -0500

    adding delimeter

--- csvdemo/Program.cs
+++ csvdemo/Program.cs
@@ -2,11 +2,18 @@
 using System.Data;
 using System.Globalization;
 using CsvHelper;
+using CsvHelper.Configuration;
 using CsvHelper.Configuration.Attributes;
+
+var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+{
+    HasHeaderRecord = false,
+    Delimiter = "|"
+};
 
 using (var reader = new StreamReader("users.csv"))
 {
-    using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+    using (var csv = new CsvReader(reader, config))
     {
         var listUsersFromCsv = csv.GetRecords<User>().ToList();
 
@@ -19,16 +26,22 @@ using (var reader = new StreamReader("users.csv"))
 
 public class User
 {
-    [Name("Username")]
-    public string UserName { get;set;}
-
-    [Name("Login email")]
+     [Name("Login email")]
+     [Index(1)]
     public string LoginEmail {get;set;}
+
+    [Name("Username")]
+    [Index(0)]
+    public string UserName { get;set;}
+   
+    [Index(2)]
     public short Identifier {get;set;}
 
+     [Index(3)]
     [Name("First name")]
     public string FirstName {get;set;}
 
+    [Index(4)]
     [Name("Last name")]
     public string LastName { get;set;}
 

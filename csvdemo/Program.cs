330dbe419bf29b2b9ab904ce1e1c60bae7ddb451
Miguel Teheran <miguelteheran@gmail.com>
2022-10-25 16:24:55 -0500

    using Getrecords method

--- csvdemo/Program.cs
+++ csvdemo/Program.cs
@@ -2,26 +2,34 @@
 using System.Data;
 using System.Globalization;
 using CsvHelper;
+using CsvHelper.Configuration.Attributes;
 
 using (var reader = new StreamReader("users.csv"))
 {
     using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
     {
-        using(var dr = new CsvDataReader(csv))
+        var listUsersFromCsv = csv.GetRecords<User>().ToList();
+
+        foreach (var item in listUsersFromCsv)
         {
-            var dt =  new DataTable();
-            dt.Columns.Add("Username" , typeof(string)); 
-            dt.Columns.Add("Login email" , typeof(string)); 
-            dt.Columns.Add("Identifier" , typeof(short)); 
-            dt.Columns.Add("First name" , typeof(string)); 
-            dt.Columns.Add("Last name" , typeof(string));   
-
-            dt.Load(dr);
-
-           for (int i = 0; i < dt.Rows.Count; i++)
-           {
-                Console.WriteLine($"{dt.Rows[i][0]} - {dt.Rows[i][2]}");
-           }
-        }
+            Console.WriteLine($"{item.UserName} - {item.Identifier}");
+        } 
     }
+}
+
+public class User
+{
+    [Name("Username")]
+    public string UserName { get;set;}
+
+    [Name("Login email")]
+    public string LoginEmail {get;set;}
+    public short Identifier {get;set;}
+
+    [Name("First name")]
+    public string FirstName {get;set;}
+
+    [Name("Last name")]
+    public string LastName { get;set;}
+
 }

efad9b373141827009cf680188b2c78719586d08
Miguel Teheran <miguelteheran@gmail.com>
2022-10-26 17:43:27 -0500

    adding attributes for Mappers

--- automapperconsole/Program.cs
+++ automapperconsole/Program.cs
@@ -1,21 +1,29 @@
 ﻿using AutoMapper;
+using AutoMapper.Configuration.Annotations;
+
 var newPerson = new Person() { Name= "Maria", LastName = "Lopez", Age=20};
 
-var configMapper = new MapperConfiguration(cfg=> cfg.CreateMap<Person, User>()
-                                                    .ForMember(dest=> dest.UserName, opt=> opt.MapFrom(src=> src.Name + src.LastName)));
+var configMapper = new MapperConfiguration(cfg=> cfg.AddMaps("automapperconsole"));
+                                                    //cfg.CreateMap<Person, User>()
+                                                    //.ForMember(dest=> dest.FirstName, opt=> opt.MapFrom(src=> src.Name))
+                                                    //.ForMember(dest=> dest.UserName, opt=> opt.MapFrom(src=> src.Name + src.LastName)));
 var mapper = new Mapper(configMapper);
 
 var newUser = mapper.Map<User>(newPerson);
 
 
-Console.WriteLine($"Nombre: {newUser.Name} ");
+Console.WriteLine($"Nombre: {newUser.FirstName} ");
 Console.WriteLine($"Apellido: {newUser.LastName} ");
 Console.WriteLine($"Usuario: {newUser.UserName} ");
 
+[AutoMap(typeof(Person))]
 public class User
 {
-    public string Name {get;set;}
+    [SourceMember("Name")]
+    public string FirstName {get;set;}
     public string LastName {get;set;}
+
+     [SourceMember("Name")]
     public string UserName {get;set;} 
 }
 

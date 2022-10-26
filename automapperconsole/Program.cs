d2317415ae90e592ccbfbfe2391626e93393c467
Miguel Teheran <miguelteheran@gmail.com>
2022-10-26 15:20:10 -0500

    adding automapper library

--- automapperconsole/Program.cs
+++ automapperconsole/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AutoMapper;
+var newPerson = new Person() { Name= "Maria", LastName = "Lopez", Age=20};
+
+var configMapper = new MapperConfiguration(cfg=> cfg.CreateMap<Person, User>());
+var mapper = new Mapper(configMapper);
+
+var newUser = mapper.Map<User>(newPerson);
+
+
+Console.WriteLine($"Nombre: {newUser.Name} ");
+Console.WriteLine($"Apellido: {newUser.LastName} ");
+Console.WriteLine($"Usuario: {newUser.UserName} ");
+
+public class User
+{
+    public string Name {get;set;}
+    public string LastName {get;set;}
+    public string UserName {get;set;} 
+}
+
+public class Person
+{
+    public string Name {get;set;}
+    public string LastName {get;set;}
+    public int Age {get;set;}
+
+}

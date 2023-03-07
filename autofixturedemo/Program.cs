26bd2820e47c64ebe70114f7a74543885afc39fb
Miguel Teheran <miguelteheran@gmail.com>
2023-03-06 21:01:41 -0500

    adding create demo with autofixture

--- autofixturedemo/Program.cs
+++ autofixturedemo/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AutoFixture;
+
+Fixture fixture = new Fixture();
+
+var user = fixture.Create<User>();
+
+Console.WriteLine($"Name: {user.Name}");
+Console.WriteLine($"Email: {user.Email}");
+Console.WriteLine($"NumberOfLogins: {user.NumberOfLogins}");
+Console.WriteLine($"DateCreated: {user.DateCreated}");
+
+public class User 
+{
+    public string Name {get;set;}
+    public string Email {get;set;}
+    public int NumberOfLogins  { get;set;}
+    public DateTime DateCreated {get;set;}
+}

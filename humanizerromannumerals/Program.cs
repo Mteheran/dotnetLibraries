c02ad2cef3c831d399b83e34310680d9b487cc50
Miguel Teheran <miguelteheran@gmail.com>
2023-03-06 20:44:54 -0500

    adding base code for AutoFixture

--- humanizerromannumerals/Program.cs
+++ humanizerromannumerals/Program.cs
@@ -1,35 +1,33 @@
-﻿using Humanizer;
-
-List<BookChapter> harryPotter = new List<BookChapter>();
+﻿List<BookChapter> harryPotter = new List<BookChapter>();
 harryPotter.Add(new BookChapter()
 {
-    ChapterNumber = "I",
+    ChapterNumber = 1,
     Title = "El Niño que Vivió"
 });
 harryPotter.Add(new BookChapter()
 {
-    ChapterNumber = "II",
+    ChapterNumber = 2,
     Title = "El Vidrio que se Desvaneció"
 });
 harryPotter.Add(new BookChapter()
 {
-    ChapterNumber = "III",
+    ChapterNumber = 3,
     Title = "Las Cartas de Nadie"
 });
 harryPotter.Add(new BookChapter()
 {
-    ChapterNumber = "IV",
+    ChapterNumber = 4,
     Title = "El Guardián de las Llaves"
 });
 
 foreach(var item in harryPotter)
 {
-    Console.WriteLine($"{item.ChapterNumber.FromRoman()}. {item.Title}");
+    Console.WriteLine($"{item.ChapterNumber}. {item.Title}");
 }
 
 
 public class BookChapter 
 {
-    public string ChapterNumber {get;set;}
+    public int ChapterNumber {get;set;}
     public string Title {get;set;}
 }

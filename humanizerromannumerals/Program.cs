c2fbf2cc66244accbabae9f739cbe4d704f48b9a
Miguel Teheran <miguelteheran@gmail.com>
2023-03-06 19:42:58 -0500

    adddin base code for humanizer  roman numerals

---
+++ humanizerromannumerals/Program.cs
@@ -0,0 +1,33 @@
+﻿List<BookChapter> harryPotter = new List<BookChapter>();
+harryPotter.Add(new BookChapter()
+{
+    ChapterNumber = 1,
+    Title = "El Niño que Vivió"
+});
+harryPotter.Add(new BookChapter()
+{
+    ChapterNumber = 2,
+    Title = "El Vidrio que se Desvaneció"
+});
+harryPotter.Add(new BookChapter()
+{
+    ChapterNumber = 3,
+    Title = "Las Cartas de Nadie"
+});
+harryPotter.Add(new BookChapter()
+{
+    ChapterNumber = 4,
+    Title = "El Guardián de las Llaves"
+});
+
+foreach(var item in harryPotter)
+{
+    Console.WriteLine($"{item.ChapterNumber}. {item.Title}");
+}
+
+
+public class BookChapter 
+{
+    public int ChapterNumber {get;set;}
+    public string Title {get;set;}
+}

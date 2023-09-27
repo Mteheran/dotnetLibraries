6f00365a4431da46eee11bf60b91e50d18f1296a
Miguel Teheran <miguelteheran@gmail.com>
2023-09-26 22:46:06 -0500

    agreando códgio para leer los archivos de un .ZIP

--- sharpzipdemo/Program.cs
+++ sharpzipdemo/Program.cs
@@ -1,14 +1,28 @@
 ﻿using ICSharpCode.SharpZipLib.Zip;
 
 //using (ZipFile zipFile = ZipFile.Create("test.zip"))
+
+// using (ZipFile zipFile = new ZipFile("test.zip"))
+// {
+//     zipFile.BeginUpdate();
+//     zipFile.Add("test2.txt");
+//     //zipFile.Add("test.txt");
+//     //zipFile.Add("data.csv");
+//     zipFile.CommitUpdate();
+//     zipFile.Close();
+// }
+
+// Console.WriteLine("File zip completed");
+
 using (ZipFile zipFile = new ZipFile("test.zip"))
 {
-    zipFile.BeginUpdate();
-    zipFile.Add("test2.txt");
-    //zipFile.Add("test.txt");
-    //zipFile.Add("data.csv");
-    zipFile.CommitUpdate();
-    zipFile.Close();
+    foreach(ZipEntry zip in zipFile)
+    {
+        Console.WriteLine(zip.Name);
+        using (StreamReader reader = new StreamReader(zipFile.GetInputStream(zip)))
+        {
+            Console.WriteLine($"Contenido: {reader.ReadToEnd()}");
+            reader.Close();
+        }
+    }
 }
-
-Console.WriteLine("File zip completed");

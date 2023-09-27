66eebfe8cd9715836250e8ce393e6997610e765e
Miguel Teheran <miguelteheran@gmail.com>
2023-09-26 22:05:44 -0500

    adding code base for zip library

---
+++ sharpzipdemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using ICSharpCode.SharpZipLib.Zip;
+
+using (ZipFile zipFile = ZipFile.Create("test.zip"))
+{
+    zipFile.BeginUpdate();
+    zipFile.Add("test.txt");
+    zipFile.Add("data.csv");
+    zipFile.CommitUpdate();
+    zipFile.Close();
+}
+
+Console.WriteLine("File zip completed!");
